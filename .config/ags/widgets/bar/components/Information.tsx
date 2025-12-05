4105f0ceb26f019e1ee7912c06d5e7f5e99b1872
Ayman Lyesri <lyesri99@gmail.com>
2025-12-05 13:10:13 +0100

    fix: update SCSS styles for popover and player widget layout

--- .config/ags/widgets/bar/components/Information.tsx
+++ .config/ags/widgets/bar/components/Information.tsx
@@ -324,74 +324,85 @@ function Mpris() {
   const players = createBinding(mpris, "players");
 
   return (
-    <menubutton>
-      <box>
-        <For each={players}>
-          {(player) => {
-            const [app] = apps.exact_query(player.entry);
-            return <image visible={!!app.iconName} iconName={app?.iconName} />;
-          }}
-        </For>
-      </box>
-      <popover>
-        <box spacing={4} orientation={Gtk.Orientation.VERTICAL}>
+    <revealer
+      revealChild={players((players) => players.length > 0)}
+      transitionDuration={globalTransition}
+      transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
+    >
+      <menubutton>
+        <box>
           <For each={players}>
-            {(player) => (
-              <box spacing={4} widthRequest={200}>
-                <box css="border-radius: 8px;">
-                  <image
-                    pixelSize={64}
-                    file={createBinding(player, "coverArt")}
-                  />
+            {(player) => {
+              const [app] = apps.exact_query(player.entry);
+              return (
+                <box spacing={5}>
+                  <image visible={!!app.iconName} iconName={app?.iconName} />
+                  <label label={createBinding(player, "title")} />
                 </box>
-                <box
-                  valign={Gtk.Align.CENTER}
-                  orientation={Gtk.Orientation.VERTICAL}
-                >
-                  <label xalign={0} label={createBinding(player, "title")} />
-                  <label xalign={0} label={createBinding(player, "artist")} />
-                </box>
-                <box hexpand halign={Gtk.Align.END}>
-                  <button
-                    onClicked={() => player.previous()}
-                    visible={createBinding(player, "canGoPrevious")}
-                  >
-                    <image iconName="media-seek-backward-symbolic" />
-                  </button>
-                  <button
-                    onClicked={() => player.play_pause()}
-                    visible={createBinding(player, "canControl")}
-                  >
-                    <box>
-                      <image
-                        iconName="media-playback-start-symbolic"
-                        visible={createBinding(
-                          player,
-                          "playbackStatus"
-                        )((s) => s === AstalMpris.PlaybackStatus.PLAYING)}
-                      />
-                      <image
-                        iconName="media-playback-pause-symbolic"
-                        visible={createBinding(
-                          player,
-                          "playbackStatus"
-                        )((s) => s !== AstalMpris.PlaybackStatus.PLAYING)}
-                      />
-                    </box>
-                  </button>
-                  <button
-                    onClicked={() => player.next()}
-                    visible={createBinding(player, "canGoNext")}
-                  >
-                    <image iconName="media-seek-forward-symbolic" />
-                  </button>
-                </box>
-              </box>
-            )}
+              );
+            }}
           </For>
         </box>
-      </popover>
-    </menubutton>
+        <popover>
+          <box spacing={4} orientation={Gtk.Orientation.VERTICAL}>
+            <For each={players}>
+              {(player) => (
+                <box class={"player"} spacing={4} widthRequest={200}>
+                  <box css="border-radius: 8px;">
+                    <image
+                      pixelSize={64}
+                      file={createBinding(player, "coverArt")}
+                    />
+                  </box>
+                  <box
+                    valign={Gtk.Align.CENTER}
+                    orientation={Gtk.Orientation.VERTICAL}
+                  >
+                    <label xalign={0} label={createBinding(player, "title")} />
+                    <label xalign={0} label={createBinding(player, "artist")} />
+                  </box>
+                  <box hexpand halign={Gtk.Align.END}>
+                    <button
+                      onClicked={() => player.previous()}
+                      visible={createBinding(player, "canGoPrevious")}
+                    >
+                      <image iconName="media-seek-backward-symbolic" />
+                    </button>
+                    <button
+                      onClicked={() => player.play_pause()}
+                      visible={createBinding(player, "canControl")}
+                    >
+                      <box>
+                        <image
+                          iconName="media-playback-start-symbolic"
+                          visible={createBinding(
+                            player,
+                            "playbackStatus"
+                          )((s) => s === AstalMpris.PlaybackStatus.PLAYING)}
+                        />
+                        <image
+                          iconName="media-playback-pause-symbolic"
+                          visible={createBinding(
+                            player,
+                            "playbackStatus"
+                          )((s) => s !== AstalMpris.PlaybackStatus.PLAYING)}
+                        />
+                      </box>
+                    </button>
+                    <button
+                      onClicked={() => player.next()}
+                      visible={createBinding(player, "canGoNext")}
+                    >
+                      <image iconName="media-seek-forward-symbolic" />
+                    </button>
+                  </box>
+                </box>
+              )}
+            </For>
+          </box>
+        </popover>
+      </menubutton>
+    </revealer>
   );
 }
 
@@ -419,6 +430,7 @@ function Bandwidth() {
     ["./assets/binaries/bandwidth"],
     (out) => {
       try {
+        print(out);
         const parsed = JSON.parse(out);
         return [parsed[0], parsed[1], parsed[2], parsed[3]];
       } catch (e) {
@@ -486,8 +498,9 @@ function Bandwidth() {
         if (downloadRevealerInstance)
           downloadRevealerInstance.reveal_child = false;
       }}
-      child={trigger}
-    />
+    >
+      {trigger}
+    </Eventbox>
   );
 
   return parent;
